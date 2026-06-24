dfc0e8a1ca1f7fc4c21dbc8de4ef83dc3c1b9ae8
shahin <shahinbavili@gmail.com>
2026-06-24 17:25:00 +0200

    feat(auth): Authentication UI error handling (#3)

--- src/app/features/auth/login/pages/login-page/login-page.ts
+++ src/app/features/auth/login/pages/login-page/login-page.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, signal} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {AuthService} from '../../../../../core/auth/auth.service';
 import {TranslatePipe} from '@ngx-translate/core';
@@ -15,19 +15,23 @@ import {TranslatePipe} from '@ngx-translate/core';
 export class LoginPage {
 
   private readonly authService = inject(AuthService);
+  readonly errorMessageKey = signal<string | null>(null);
 
   readonly loginForm = new FormGroup({
     email: new FormControl('', {
       nonNullable: true,
       validators: [Validators.required, Validators.email],
+      updateOn: 'blur',
     }),
     password: new FormControl('', {
       nonNullable: true,
       validators: [Validators.required],
+      updateOn: 'blur',
     }),
   });
 
   onSubmit(): void {
+    this.errorMessageKey.set(null);
     if (this.loginForm.invalid) {
       this.loginForm.markAllAsTouched();
       return;
@@ -35,7 +39,17 @@ export class LoginPage {
 
     this.authService.login(this.loginForm.getRawValue()).subscribe({
       error: error => {
-        console.log('Login failed', error);
+        if (error.status === 401) {
+          this.errorMessageKey.set('auth.errors.invalidCredentials');
+          return;
+        }
+
+        if (error.statut === 0) {
+          this.errorMessageKey.set('auth.errors.network');
+          return;
+        }
+
+        this.errorMessageKey.set('auth.errors.unknown');
       }
     })
   }
