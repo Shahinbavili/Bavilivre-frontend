713c52ffa49fc681c950b213f21cd8e5d6c6722f
shahin <shahinbavili@gmail.com>
2026-07-19 16:43:21 +0200

    feat(book): creat page and form for add book page (#44)

---
+++ src/app/features/books/add-book/pages/add-book-page/add-book-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-book-page',
+  imports: [],
+  templateUrl: './add-book-page.html',
+  styleUrl: './add-book-page.scss',
+})
+export class AddBookPage {}
