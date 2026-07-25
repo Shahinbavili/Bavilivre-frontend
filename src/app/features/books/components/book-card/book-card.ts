3ef57071d6003bab46e058c2ecd28396f97d4a4d
shahin <shahinbavili@gmail.com>
2026-07-25 11:53:40 +0200

    feat(books): edit book and return to catalog page (#45)

--- src/app/features/books/components/book-card/book-card.ts
+++ src/app/features/books/components/book-card/book-card.ts
@@ -1,14 +1,19 @@
-import {Component, input} from '@angular/core';
+import {Component, inject, input} from '@angular/core';
 import {Book} from '../../../../core/models/book.model';
 import {DatePipe} from '@angular/common';
 import {TranslatePipe} from '@ngx-translate/core';
+import {AuthService} from '../../../../core/auth/auth.service';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-book-card',
-  imports: [DatePipe, TranslatePipe],
+  imports: [DatePipe, TranslatePipe, RouterLink],
   templateUrl: './book-card.html',
   styleUrl: './book-card.scss',
 })
 export class BookCard {
+
+  readonly authService = inject(AuthService);
+  readonly currentUser = this.authService.currentUser;
   readonly book = input.required<Book>();
 }
